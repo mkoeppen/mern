52cd74f3220da659b8e8b0d39942bff56ac5aae4
Markus <mkoeppen@posteo.de>
2017-08-16 21:33:41 +0200

    Create Issue Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,76 @@
-const contentNode = document.getElementById("contents");
+const contentNode =document.getElementById("contents");
 
-const continents = ["Africa","America","Asia","Australia","Europe"];
-const message = continents.map(c => `Hello ${c}!`).join(" ");
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for IssueFilter</div>
+        );
+    }
+}
 
-//const components = <h1 className="headline">Hello World!</h1>;
-const components = <p>{message}</p>;
-ReactDOM.render(components, contentNode);
+class IssueRow extends React.Component {
+    render() {
+        const borderedStyle = { border: "1px solid silver", padding: 4 };
+        return (
+            <tr>
+                <td style={borderedStyle}>{this.props.issue_id}</td>
+                <td style={borderedStyle}>{this.props.issue_title}</td>
+            </tr>
+        );
+    }
+}
+
+IssueRow.propTypes = {
+    issue_id: React.PropTypes.number.isRequired,
+    issue_title: React.PropTypes.string
+}
+
+IssueRow.defaultProps = {
+    issue_title: "-- no title --",
+}
+
+class IssueTable extends React.Component {
+    render() {
+        const borderedStyle = { border: "1px solid silver", padding: 6 };
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th style={borderedStyle}>Id</th>
+                        <th style={borderedStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow issue_id={1} issue_title="Error in console when clicking Add" />
+                    <IssueRow issue_id={2} issue_title="Missing bottom border on panel" />
+                    <IssueRow issue_id={3} />
+                </tbody>
+            </table>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for IssueAdd</div>
+        );
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
