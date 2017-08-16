329dec8c35d9166cd0940f8f02919eec2ca87c08
Markus <mkoeppen@posteo.de>
2017-08-16 22:13:49 +0200

    Extend issue table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 const contentNode =document.getElementById("contents");
 
+const issues = [
+    {
+        id: 1,
+        status: "Open",
+        owner: "Ravan",
+        created: new Date("2017-08-15"),
+        effort: 5,
+        completionDate: undefined,
+        title: "Error in console when clicking Add"
+    },
+    {
+        id: 2,
+        status: "Assigned",
+        owner: "Eddie",
+        created: new Date("2017-08-16"),
+        effort: 14,
+        completionDate: new Date("2017-08-30"),
+        title: "Missing bottom border on panel"
+    }
+]
+
 class IssueFilter extends React.Component {
     render() {
         return (
@@ -10,41 +31,49 @@ class IssueFilter extends React.Component {
 
 class IssueRow extends React.Component {
     render() {
-        const borderedStyle = { border: "1px solid silver", padding: 4 };
+        const issue = this.props.issue;
+
         return (
             <tr>
-                <td style={borderedStyle}>{this.props.issue_id}</td>
-                <td style={borderedStyle}>{this.props.issue_title}</td>
+                <td>{issue.id}</td>
+                <td>{issue.status}</td>
+                <td>{issue.owner}</td>
+                <td>{issue.created.toDateString()}</td>
+                <td>{issue.effort}</td>
+                <td>{issue.completionDate ? issue.completionDate.toDateString() : ""}</td>
+                <td>{issue.title}</td>
             </tr>
         );
     }
 }
 
 IssueRow.propTypes = {
-    issue_id: React.PropTypes.number.isRequired,
+    issue: React.PropTypes.number.isRequired,
     issue_title: React.PropTypes.string
 }
 
 IssueRow.defaultProps = {
-    issue_title: "-- no title --",
+    children: "-- no title --",
 }
 
 class IssueTable extends React.Component {
     render() {
-        const borderedStyle = { border: "1px solid silver", padding: 6 };
+        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
+        
         return (
-            <table>
+            <table className="bordered-table">
                 <thead>
                     <tr>
-                        <th style={borderedStyle}>Id</th>
-                        <th style={borderedStyle}>Title</th>
+                        <th>Id</th>
+                        <th>Status</th>
+                        <th>Owner</th>
+                        <th>Created</th>
+                        <th>Effort</th>
+                        <th>Completion Date</th>
+                        <th>Title</th>                        
                     </tr>
                 </thead>
-                <tbody>
-                    <IssueRow issue_id={1} issue_title="Error in console when clicking Add" />
-                    <IssueRow issue_id={2} issue_title="Missing bottom border on panel" />
-                    <IssueRow issue_id={3} />
-                </tbody>
+                <tbody>{issueRows}</tbody>
             </table>
         );
     }
@@ -65,7 +94,7 @@ class IssueList extends React.Component {
                 <h1>Issue Tracker</h1>
                 <IssueFilter />
                 <hr />
-                <IssueTable />
+                <IssueTable issues={issues} />
                 <hr />
                 <IssueAdd />
             </div>
