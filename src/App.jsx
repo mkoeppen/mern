af3cd9d22a678c8575832d3388dd1455dda571e5
Markus <mkoeppen@posteo.de>
2017-08-18 18:53:57 +0200

    First event handler

--- src/App.jsx
+++ src/App.jsx
@@ -99,6 +99,8 @@ class IssueList extends React.Component {
     constructor() {
         super();
         this.state = { issues: [] };
+
+        this.createTestIssue = this.createTestIssue.bind(this);
         setTimeout(this.createTestIssue.bind(this), 2000);
     }
 
@@ -136,6 +138,7 @@ class IssueList extends React.Component {
                 <IssueFilter />
                 <hr />
                 <IssueTable issues={this.state.issues} />
+                <button onClick={this.createTestIssue}>Add</button>
                 <hr />
                 <IssueAdd />
             </div>
